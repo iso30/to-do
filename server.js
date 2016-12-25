7ad6909e11a4f729439295f3029b9d7070574629
Mohammed Azad <varsity.azad@gmail.com>
2016-12-25 23:55:50 +0000

    added test db

--- server.js
+++ server.js
@@ -1,29 +1,96 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
+
 var http = require('http');
 var path = require('path');
+var fs = require('fs');
 
 var async = require('async');
 var express = require('express');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
+//db config
+var pg = require('pg');
+var config = {
+  user: process.env.PGUSER, //env var: PGUSER
+  database: 'test', //env var: PGDATABASE
+  password: 'pgpassword', //env var: PGPASSWORD
+  host: process.env.IP, // Server hosting the postgres database
+  port: process.env.PGPORT, //env var: PGPORT
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, //
+};
+var pool = new pg.Pool(config);
+var scheema = fs.readFileSync('./db/scheema.sql').toString();
+var bodyParser = require('body-parser');
+
 var router = express();
 var server = http.createServer(router);
-var viewPath = "./views/"
+
+router.use(express.static(__dirname + '/views'));
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+router.use(bodyParser.json());
 
 router.get("/",function(req,res){
-  res.sendfile(viewPath + "index.html")
+  res.sendfile("/views/index.html")
 });
 
+router.post("/",function(req,res){
+  pool.connect(function(err,client,done){
+    if(err){
+      return console.error('error fetching client from pool',err);
+    }
+    client.query('INSERT INTO tab (name) VALUES($1)',[req.body.name],function(err,result){
+      done();
+      if(err){
+        return console.error('error running query',err);
+      }
+    });
+  });
+  pool.on('error',function(err,client){
+    console.error('ide client error', err.message, err.stack);
+  })
+  res.redirect("/")
+});
+
+router.get("/list", function(req,res){
+  pool.connect(function(err, client, done) {
+      if(err){
+        return console.error("error fetching client from pool",err);
+      }
+      client.query('SELECT * FROM tab',function(err,result){
+        done();
+        res.json(result.rows);
+      })
+  });
+  pool.on('error',function(err,client){
+    console.error('idle client error', err.message,err.stack);
+  })
+});
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   console.log(("Connected"))
+  pool.connect(function(err,client,done){
+    if(err){
+      return console.error('error fetching client from pool',err);
+    }
+    client.query(scheema,function(err,result){
+      done();
+      
+      if(err){
+        return console.error('error running query',err);
+      }
+    });
+    pool.on('error',function(err,client){
+      console.error('idle client error',err.message,err.stack);
+    })
+  })
 });
